52aedceca8097b2d004cde5d58a55c9499d87b17
readf0x <davis.a.forsythe@gmail.com>
2025-01-19 11:03:43 -0600

    added delta config

--- global/home/default.nix
+++ global/home/default.nix
@@ -66,6 +66,45 @@
         state_file "~/.mpd/state"
         sticker_file "~/.mpd/sticker.sql"
       '';
+      ".gitconfig".text = ''
+        [delta]
+          # author: https://github.com/anthony-halim
+          dark = true
+          syntax-theme = Nord
+          file-added-label = [+]
+          file-copied-label = [==]
+          file-modified-label = [*]
+          file-removed-label = [-]
+          file-renamed-label = [->]
+          file-style = omit
+          hunk-header-decoration-style = 
+          hunk-header-file-style = blue italic
+          hunk-header-line-number-style = yellow box bold
+          hunk-header-style = file line-number syntax bold italic
+          plus-style = green
+          plus-emph-style = black green
+          minus-style = red
+          minus-emph-style = black red
+          line-numbers = true
+          line-numbers-minus-style = red
+          line-numbers-plus-style = green
+          line-numbers-left-format = "{nm} "
+          line-numbers-right-format = "{np} "
+          line-numbers-left-style = cyan
+          line-numbers-right-style = cyan
+          line-numbers-zero-style = blue dim
+          zero-style = syntax
+          whitespace-error-style = black bold
+          blame-code-style = syntax
+          blame-format = "{author:<18} {commit:<6} {timestamp:<15}"
+          blame-palette = brightcyan cyan cyan
+          merge-conflict-begin-symbol = ~
+          merge-conflict-end-symbol = ~
+          merge-conflict-ours-diff-header-style = yellow bold
+          merge-conflict-ours-diff-header-decoration-style = blue box
+          merge-conflict-theirs-diff-header-style = yellow bold
+          merge-conflict-theirs-diff-header-decoration-style = blue box
+      '';
     };
 
     sessionVariables = {
