85d2b8f09d3a1dd847cca8ded1d36e6ba63cc383
readf0x <davis.a.forsythe@gmail.com>
2025-01-20 15:37:09 -0600

    laptop settings

--- hosts/readf0x@Loki-IV/home/default.nix
+++ hosts/readf0x@Loki-IV/home/default.nix
@@ -1,5 +1,15 @@
-{ ... }: {
-  programs.kitty.extraConfig = ''
-    font_size 12
-  '';
+{ lib, ... }: {
+  programs = {
+    kitty.extraConfig = ''
+      font_size 12
+    '';
+    waybar.settings.bar = {
+      modules-right = lib.mkForce [
+        "network"
+	"battery"
+	"wireplumber"
+	"clock"
+      ];
+    };
+  };
 }
