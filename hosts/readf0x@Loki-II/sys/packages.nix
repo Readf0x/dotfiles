22a53932feb5f7e0f5aecea0b87d707c92a498ef
readf0x <davis.a.forsythe@gmail.com>
2025-03-20 17:19:20 -0500

    added feral gamemode

--- hosts/readf0x@Loki-II/sys/packages.nix
+++ hosts/readf0x@Loki-II/sys/packages.nix
@@ -14,5 +14,20 @@
       gpuOverclock.enable = true;
     };
     virt-manager.enable = true;
+    gamemode = {
+      enable = true;
+      settings = {
+        general.renice = 10;
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          amd_performance_level = "high";
+        };
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send --icon=applications-games 'GameMode Started'";
+          end = "${pkgs.libnotify}/bin/notify-send --icon=applications-games 'GameMode Ended'";
+        };
+      };
+    };
   };
 }
