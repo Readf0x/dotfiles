18f6528f8a4ae84155b11c5a663bd1d71fcb44a2
readf0x <davis.a.forsythe@gmail.com>
2025-04-21 14:21:44 -0500

    fixed upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if [[ $(git fetch) -eq "" ]]; then
+if [[ $(git rev-list origin/$(git branch --show-current 2>/dev/null) --not HEAD --count) -eq '0' ]]; then
   nix flake update && \
   ./switch.sh && \
   ./global/scripts/nix-clean
