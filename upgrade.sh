0177151e27988e9ef7d0fcf24c27ddfd65bb39c0
readf0x <davis.a.forsythe@gmail.com>
2025-04-21 13:26:21 -0500

    updated

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 
-nix flake update && \
-./switch.sh && \
-./global/scripts/nix-clean
+if [[ $(git fetch) -eq "" ]]; then
+  nix flake update && \
+  ./switch.sh && \
+  ./global/scripts/nix-clean
+else
+  print "Repo is not up to date."
+fi
 
