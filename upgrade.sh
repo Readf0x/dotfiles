6de9193aea4cf2be3c03acc84b85ecaa0b63c94d
readf0x <davis.a.forsythe@gmail.com>
2025-04-22 16:46:18 -0500

    script cleaning

--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env zsh
 
-if [[ $(git rev-list origin/$(git branch --show-current 2>/dev/null) --not HEAD --count) -eq '0' ]]; then
-  nix flake update && \
-  ./switch.sh && \
-  ./global/scripts/nix-clean
+if [[ $1 == -h ]] || [[ $1 == --help ]]; then
+  msg="$(sed -e 's/^[ ]*| //m' <<'--------------------'
+    | A script for upgrading this flake
+    | 
+    | %UUsage:%u %Bupgrade.sh%b [ARGS]
+    | 
+    | %UArguments:%u
+    |   %B-c%b\tClean up store after upgrading (useful for low storage systems)
+--------------------
+    )"
+  print -P $msg
 else
-  print "Repo is not up to date."
+  if [[ $(git rev-list origin/$(git branch --show-current 2>/dev/null) --not HEAD --count) == '0' ]]; then
+    nix flake update && \
+    ./switch.sh && \
+    if [[ $1 == "-c" ]]; then
+      ./global/scripts/nix-clean
+    fi
+  else
+    print "Repo is not up to date."
+  fi
 fi
 
