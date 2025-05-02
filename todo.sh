71295cd9cd881fa87d8862de54f9e857966f171d
readf0x <davis.a.forsythe@gmail.com>
2025-05-01 21:08:28 -0500

    updated todo script

--- todo.sh
+++ todo.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env zsh
+#! /usr/bin/env nix-shell
+#! nix-shell -i zsh -p md4c ripgrep bat xdg-utils
 
 function meow() {
-  print "\# \[TODO\]"
-  rg '\[TODO\]' | sed 's/.*:\s*# //' | sed 's/\[TODO\]/-/'
+  print "\# \[TODO\]";
+
+  items=(${(f)"$(rg '\[TODO\]')"})
+  for i in $items; do
+    print "$i" | sed 's/^\([a-zA-Z0-9\/\.]*\)/*\1*/' | sed 's/:\s*# /:\n/' | sed 's/\[TODO\]/-/'
+    print
+  done
 }
 
-meow | bat -plmd
+if [[ $1 == '--html' ]]; then
+  meow | md2html -o /tmp/todo.html
+  xdg-open /tmp/todo.html
+else
+  meow | bat -plmd
+fi
 
