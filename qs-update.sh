fefaf122a63a281fd8865347e1d9586ec1fbe902
readf0x <davis.a.forsythe@gmail.com>
2025-06-15 10:33:33 -0500

    finally fix qs-update
    hasn't been tested, this commit might get removed

--- qs-update.sh
+++ qs-update.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env zsh
 
+metadata=$(nix flake metadata 2>&1 | rg -oe 'github:readf0x\/quickshell\/[^\?]*')
+commit=${metadata#'github:readf0x/quickshell/'}
+latest=$(curl 'https://api.github.com/repos/readf0x/quickshell/commits?per_page=1' 2>/dev/null | jq '.[].sha' -r)
+
 function update() {
-  commits=("${(@f)$(nix flake update neoshell 2>&1 | rg -o '[0-9a-f]{40}')}")
-  git commit -m 'update quickshell' -m "${commits[1]} -> ${commits[2]}" -m $1
+  nix flake update neoshell
+  git add flake.lock
+  git commit -m 'update quickshell' -m "${commit:0:7} -> ${latest:0:7}"
 }
 
 if [[ $1 = ('-f'|'--force') ]]; then
   update
+  exit
 fi
 
-if nix flake metadata 2>/dev/null | rg -F "$(nix flake show 'github:readf0x/quickshell' 2>/dev/null | head -n 1 | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g')"; then
+if [[ $commit = $latest ]]; then
   print "Already up to date"
-elif git status --porcelain | grep -q .; then
+elif ! git status --porcelain | rg -q .; then
   update
 else
   print "Repo has working changes, actions would commit staged changes and/or could depend on unstaged changes."
