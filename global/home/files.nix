316288853b85701b9265a07c2a11be4e8986ecdd
readf0x <davis.a.forsythe@gmail.com>
2025-06-30 16:01:28 -0500

    add qtct config to files

--- global/home/files.nix
+++ global/home/files.nix
@@ -76,6 +76,74 @@
     ".config/bat/config".text = ''
       --theme="base16"
     '';
+    ".config/qt5ct/qt5ct.conf".text = ''
+      [Appearance]
+      color_scheme_path=/home/readf0x/.config/qt5ct/style-colors.conf
+      custom_palette=true
+      icon_theme=Colloid-Dark
+      standard_dialogs=default
+      style=kvantum-dark
+
+      [Fonts]
+      fixed="Courier,10,-1,5,50,0,0,0,0,0,Regular"
+      general="Ubuntu,10,-1,5,50,0,0,0,0,0,Regular"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+
+      [SettingsWindow]
+      geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\a\x80\0\0\0\0\0\0\v8\0\0\x4\xf\0\0\a\x80\0\0\0\0\0\0\v?\0\0\x4\x17\0\0\0\0\x2\0\0\0\a\x80\0\0\a\x80\0\0\0\0\0\0\v8\0\0\x4\xf)
+
+      [Troubleshooting]
+      force_raster_widgets=1
+      ignored_applications=@Invalid()
+    '';
+    ".config/qt6ct/qt6ct.conf".text = ''
+      [Appearance]
+      color_scheme_path=/home/readf0x/.config/qt6ct/style-colors.conf
+      custom_palette=true
+      icon_theme=Colloid-Dark
+      standard_dialogs=default
+      style=kvantum-dark
+
+      [Fonts]
+      fixed="Courier,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
+      general="Ubuntu,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+
+      [SettingsWindow]
+      geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\a\x80\0\0\0\0\0\0\v8\0\0\x4\xf\0\0\a\x80\0\0\0\0\0\0\v8\0\0\x4\xf\0\0\0\0\x2\0\0\0\a\x80\0\0\a\x80\0\0\0\0\0\0\v8\0\0\x4\xf)
+
+      [Troubleshooting]
+      force_raster_widgets=1
+      ignored_applications=/run/current-system/sw/bin/kdenlive
+    '';
     # ".ssh/config".text = ''
     #   Host Loki-II
     #     HostName 10.1.11.104
