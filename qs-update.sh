21a4d0621507282a067cb026d981b0b5d843d194
readf0x <davis.a.forsythe@gmail.com>
2025-07-22 09:52:39 -0500

    simplify qs-update

--- qs-update.sh
+++ qs-update.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
-metadata=$(nix flake metadata 2>&1 | rg -oe 'github:readf0x\/quickshell\/[^\?]*')
-commit=${metadata#'github:readf0x/quickshell/'}
+commit=$(cat flake.lock | jq -r '.nodes.neoshell.locked.rev')
 latest=$(curl 'https://api.github.com/repos/readf0x/quickshell/commits?per_page=1' 2>/dev/null | jq '.[].sha' -r)
 
 function update() {
@@ -10,16 +9,8 @@ function update() {
   git commit -m 'update quickshell' -m "${commit:0:7} -> ${latest:0:7}"
 }
 
-if [[ $1 = ('-f'|'--force') ]]; then
-  update
-  exit
-fi
-
 if [[ $commit = $latest ]]; then
   print "Already up to date"
-elif ! git status --porcelain | rg -q .; then
-  update
 else
-  print "Repo has working changes, actions would commit staged changes and/or could depend on unstaged changes."
-  print "Run with -f, --force to run actions anyway."
+  update
 fi
