0887fae13724b08a7f93cd23931529c61ef353e5
readf0x <davis.a.forsythe@gmail.com>
2025-07-30 16:05:43 -0500

    improve switch script

--- switch.sh
+++ switch.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
 
-nh os switch . -- $@
-nh home switch . -- $@
+SUB=()
+if ! [[ $HOSTNAME = "Loki-IV" ]] && ping loki4 -w 1 2>&1 >/dev/null; then
+  SUB+='http://loki4:5000'
+elif ! [[ $HOSTNAME = "Loki-II" ]] && ping loki2 -w 1 2>&1 >/dev/null; then
+  SUB+='http://loki2:5000'
+fi
+
+if [[ ${#SUB} > 0 ]]; then
+  nh os switch . -- --option substituters "$SUB" $@
+  nh home switch . -- --option substituters "$SUB" $@
+else
+  nh os switch . -- $@
+  nh home switch . -- $@
+fi
 
