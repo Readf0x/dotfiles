297287b29495ff016d51879f0512e3e0c76e6f4f
readf0x <davis.a.forsythe@gmail.com>
2025-10-30 14:41:46 -0500

    update wl-shimeji

--- packages/wl-shimeji.nix
+++ packages/wl-shimeji.nix
@@ -2,7 +2,6 @@
   lib,
   fetchFromGitHub,
   stdenv,
-  makeWrapper,
   pkg-config,
   which,
   python3,
@@ -10,63 +9,60 @@
   wayland-scanner,
   wayland-protocols,
   libarchive,
-  uthash
-}: stdenv.mkDerivation (finalAttrs: {
-  name = "wl-shimeji";
-  pname = "shimejictl";
-  version = "";
+  uthash,
+  makeWrapper,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wl-shimeji";
+  version = "0.0.2";
 
   src = fetchFromGitHub {
     owner = "CluelessCatBurger";
     repo = "wl_shimeji";
-    rev = "9ad350f23d2121a4717f6215b32221d77543e0ca";
-    hash = "sha256-ME/Mbasi6DfJ0N9OX3imy9VPrZhgHwBn271+TbUDT3M=";
+    rev = "bf0150a313c3df271d249d3e14a17b48193f67e8";
+    hash = "sha256-ag3MHyyfo+2yZxPSBZgBsVZ4a6nLSv1KMUcYS34lIiM=";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [
-    wayland-protocols
-    wayland-scanner
-    libarchive
-    uthash
     pkg-config
     which
     python3
-    makeWrapper
   ];
   buildInputs = [
     wayland
-    libarchive.lib
+    wayland-protocols
+    wayland-scanner
+    uthash
+    libarchive
+    makeWrapper
+    (python3.withPackages (ps: [ ps.pillow ]))
   ];
 
-  outputs = [ "out" "dev" ];
+  outputs = [
+    "out"
+    "dev"
+  ];
 
-  buildPhase = ''
-    make
-  '';
-  installPhase = let
-    python = python3.withPackages (ps: with ps; [ pillow ]);
-  in ''
-    DESTDIR=$out PREFIX="" make install
-    mkdir -p $dev
-    mv $out/include $dev
-    cat <<EOF > $out/share/systemd/user/wl_shimeji.service
-    [Unit]
-    Description=wl_shimeji's overlay daemon
+  installFlags = [
+    "PREFIX=$(out)"
+  ];
 
-    [Service]
-    Type=simple
-    ExecStart=$out/bin/shimeji-overlayd
-    EOF
+  postInstall = ''
     wrapProgram $out/bin/shimejictl \
-      --set PYTHONPATH "${python}/lib/python3.11/site-packages" \
-      --set XDG_CURRENT_DESKTOP "" \
-      --prefix PATH : ${lib.makeBinPath [ python ]}
+      --set XDG_CURRENT_DESKTOP ""
+  '';
+
+  postPatch = ''
+    substituteInPlace systemd/wl_shimeji.service \
+      --replace-fail "/usr/bin" "$out"
   '';
 
   meta = {
     description = "Shimeji reimplementation for Wayland in C";
     homepage = "https://github.com/CluelessCatBurger/wl_shimeji/";
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ readf0x ];
+    platforms = lib.platforms.linux;
   };
 })
