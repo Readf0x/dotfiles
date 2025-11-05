89fe54187a10f0081365b91087b4d466e51bd675
readf0x <davis.a.forsythe@gmail.com>
2025-11-05 12:02:42 -0600

    kitty uses fish
    also find out what git integration is

--- global/home/terminal.nix
+++ global/home/terminal.nix
@@ -234,8 +234,10 @@
     };
     kitty = {
       enable = true;
-      shellIntegration.enableZshIntegration = true;
+      enableGitIntegration = true;
       extraConfig = ''
+        shell fish
+
         enable_audio_bell no
         visual_bell_duration 0.1
         visual_bell_color #3a2828
