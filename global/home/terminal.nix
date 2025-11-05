966f2ebede86cfb7253eee3fc439a06ffdf61eb1
readf0x <davis.a.forsythe@gmail.com>
2025-11-05 13:34:21 -0600

    fix kitty shell

--- global/home/terminal.nix
+++ global/home/terminal.nix
@@ -241,6 +241,8 @@
       enable = true;
       enableGitIntegration = true;
       extraConfig = ''
+        shell fish
+
         enable_audio_bell no
         visual_bell_duration 0.1
         visual_bell_color #3a2828
